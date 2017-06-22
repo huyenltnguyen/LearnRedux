9a4f3f52093fd870e6d8275d1c5f0aad16dbbedc
huyenltnguyen <huyenltnguyen@outlook.com>
2017-06-23 03:36:08 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # LearnRedux
+
+[Modern React with Redux](https://www.udemy.com/react-redux/) Course exercise
+
+### Getting Started
+
+There are two methods for getting started with this repo.
+
+#### CLI
+
+```
+> npm install
+> npm start
+```
+
+(App is on port 8080)
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// jshint ignore: start
+
+import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
+
+const rootReducer = combineReducers({
+	books: BooksReducer,
+	activeBook: ActiveBook
+});
+
+export default rootReducer;
