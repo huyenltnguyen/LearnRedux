f67d9b71d66de5390de208edb3a13fde275e70a4
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-09-28 23:17:10 +0700

    setup feat/rewrite branch

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export function selectBook(book) {
-  // selectBook is an ActionCreator.
-  // It needs to return an action - an object with a type property
-  return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  };
-}
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    {title: 'Javasript: The Godd Parts', page: 200},
-    {title: 'Harry Potter', page: 578},
-    {title: 'The Dark Tower', page: 103},
-    {title: 'Eloquent Ruby', page: 1}
-  ];
-}
